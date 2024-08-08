28eabb1a5956e07b36548608e3111ec28595fca3
abdo3ssa <abdelrahman.mohamed@99.zone>
2024-08-08 22:11:59 +0300

    First commit

---
+++ app/Console/Commands/CreateCrudCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateCrudCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:crud';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new CRUD.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
